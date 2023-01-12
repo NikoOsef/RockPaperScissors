6aa5f4e41b62e057a2da522ecbfcde186b02a244
Niko <pricelesstoby@gmail.com>
2023-01-13 02:38:13 +0400

    :: Testing functions - starting CSS

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let playerChoice;
 let computerNum;
-let score;
+let computerChoice;
+let playerScore;
+let compScore;
+
 
     
 function getComputerChoice(){
-        let randomNum = Math.floor(Math.random() * 3 + 1);
-        if(randomNum == 1){
-            computerNum = 0;
-        }else if(randomNum == 2){
-            computerNum = 1;
-        }else if(randomNum == 3){
-            computerNum = 2;
-        }
-    return computerNum;
+    let randomNum = Math.floor(Math.random() * 3 + 1);
+    if(randomNum == 1){
+        computerNum = 0; //Rock
+    }else if(randomNum == 2){
+        computerNum = 1; //Paper
+    }else if(randomNum == 3){
+        computerNum = 2; //Scissors
+    }
+     return computerNum;
 }
 
-let computerChoice = getComputerChoice();
+
+
+
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
@@ -23,12 +28,25 @@ buttons.forEach((button) => {
     if(button.textContent == "Rock"){
         playerChoice = 0;
         }else if(button.textContent == "Paper"){
-        playerchoice = 1;
+        playerChoice = 1;
     }else if(button.textContent == "Scissors"){
-        playerchoice = 2;
+        playerChoice = 2;
     }
+        computerChoice = getComputerChoice();
+        playerRound(playerChoice, computerChoice);
   });
-    
+  
 
 });
+
+function playerRound(player, computer){
+    if(player == 0 && computer == 1){
+       alert("Hello");
+
+    }else if(player == 0 && computer == 2){
+        
+    }else if(player == 0 && computer == 2){
+        alert("Scissors");
+    }
+}
 
